5fdb1d6d5a0b65e48b6a118c232f236431498b16
byungi <zeronin2@gmail.com>
2021-07-27 23:59:40 +0900

    wip

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import usePrevious from 'use-previous'
+
+export default function Refereshable({ children }: { children: ReactNode }) {
+    const curr = useLocation()
+    const prev = usePrevious(curr)
+    const [isBlank, setIsBlank] = useState(false)
+
+    const isRefreshRender =
+        prev &&
+        prev.pathname === curr.pathname &&
+        prev.search === curr.search &&
+        prev.state === curr.state &&
+        prev.key !== curr.key
+
+    useEffect(() => {
+        if (isRefreshRender) {
+            setIsBlank(true)
+        } else if (isBlank) {
+            setIsBlank(false)
+        }
+    }, [isRefreshRender, isBlank])
+
+    return isBlank || children
+}
