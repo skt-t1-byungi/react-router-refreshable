da4b1e5ddc71711ed9adf387885544a3d505b8b8
byungi <zeronin2@gmail.com>
2021-08-01 22:32:50 +0900

    feat: useIsRefreshingRef

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,64 @@
-import React, { PropsWithChildren, useLayoutEffect, useState, createContext, useRef, useContext } from 'react'
+import React, { PropsWithChildren, useLayoutEffect, useEffect, createContext, useRef, useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import usePrevious from 'use-previous'
+import useForceUpdate from 'use-force-update'
 
-const ctx = createContext(() => {})
+const ctx = createContext({
+    isRefreshingRef: { current: false },
+    counterFx() {},
+})
 
 export default function Refreshable({ children, on }: PropsWithChildren<{ on?: () => void }>) {
-    const listenerRef = useRef(on)
-    listenerRef.current = on
-
     const counterRef = useRef(0)
-    const counterFx = useRef(() => {
-        counterRef.current++
-        return () => {
-            counterRef.current--
-        }
+
+    const isRefreshingRef = useRef(false)
+    const currCtxRef = useRef({
+        isRefreshingRef,
+        counterFx() {
+            counterRef.current++
+            return () => {
+                counterRef.current--
+            }
+        },
     }).current
 
-    const parentCounterFx = useContext(ctx)
-    useLayoutEffect(parentCounterFx, [])
+    const parentCtx = useContext(ctx)
+    useLayoutEffect(parentCtx.counterFx, [])
 
     const curr = useLocation()
     const prev = usePrevious(curr)
-    const [isBlank, setIsBlank] = useState(false)
 
     const isRefreshRender =
+        counterRef.current === 0 &&
         prev &&
         prev.pathname === curr.pathname &&
         prev.search === curr.search &&
         prev.state === curr.state &&
         prev.key !== curr.key
 
-    useLayoutEffect(() => {
-        if (counterRef.current === 0 && isRefreshRender) {
-            setIsBlank(true)
-            listenerRef.current?.()
-        } else if (isBlank) {
-            setIsBlank(false)
+    if (isRefreshRender) {
+        isRefreshingRef.current = true
+    }
+    useEffect(() => {
+        if (!isRefreshRender && isRefreshingRef.current) {
+            isRefreshingRef.current = false
         }
-    }, [isRefreshRender, isBlank])
+    })
 
-    return <ctx.Provider value={counterFx}>{isBlank || children}</ctx.Provider>
+    const forceUpdate = useForceUpdate()
+    const listenerRef = useRef(on)
+    listenerRef.current = on
+
+    useLayoutEffect(() => {
+        if (isRefreshRender) {
+            forceUpdate()
+            listenerRef.current?.()
+        }
+    }, [isRefreshRender])
+
+    return <ctx.Provider value={currCtxRef}>{isRefreshRender || children}</ctx.Provider>
+}
+
+export function useIsRefreshingRef() {
+    return useContext(ctx).isRefreshingRef
 }
